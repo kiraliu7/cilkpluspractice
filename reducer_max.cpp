362a5e5b6094008f485af41611186b8a719207ae
kiraliu7 <70236456+kiraliu7@users.noreply.github.com>
2021-02-28 21:06:41 -0600

    Update reducer_max.cpp

--- reducer_max.cpp
+++ reducer_max.cpp
@@ -6,11 +6,14 @@
 #include <cilk/reducer_max.h>
 #include <time.h>       /* time */
 
-cilk::reducer_max<int> maxint;
-cilk_for(int i=8; i>=1; 1--){
-    cilk_for(int j=8; j>=1; j--){
-        maxint=(9-i)*(9-j);
+int main(int argc, const char* argv[])
+{
+    cilk::reducer_max<int> maxint;
+    cilk_for(int i=8; i>=1; 1--){
+        cilk_for(int j=8; j>=1; j--){
+            maxint=(9-i)*(9-j);
+        }
     }
-}
 
-printf("maxint is: %d", maxint);
+    printf("maxint is: %d", maxint);
+}
