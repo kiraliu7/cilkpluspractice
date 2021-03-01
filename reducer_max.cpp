f10c58506d721defce0cb747c7013e1c123407a2
kiraliu7 <70236456+kiraliu7@users.noreply.github.com>
2021-02-28 21:20:02 -0600

    Update reducer_max.cpp

--- reducer_max.cpp
+++ reducer_max.cpp
@@ -9,7 +9,7 @@
 int main(int argc, const char* argv[])
 {
     int maxint=0;
-    cilk_for(int i=8; i>=1; 1--){
+    cilk_for(int i=8; i>=1; i--){
         cilk_for(int j=8; j>=1; j--){
             cilk::max_of((9-i)*(9-j), maxint);
         }
